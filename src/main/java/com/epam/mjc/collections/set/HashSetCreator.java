0586776299addbf8ec93c77886ffe984862d6897
Jurijs <j.lacis.code@gmail.com>
2024-01-02 19:55:39 +0800

    complete

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,36 @@
 package com.epam.mjc.collections.set;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+
+        for(Integer i : sourceList){
+            if(i % 2 == 0){
+                addEven(result,i);
+            }else {
+                addOdd(result, i);
+            }
+        }
+
+        return result;
+    }
+
+    private void addEven(HashSet<Integer> result, Integer i) {
+        List<Integer> itemsToAdd = new ArrayList<>();
+        while(i != 0){
+            itemsToAdd.add(i);
+            i /= 2;
+        }
+
+        result.addAll(itemsToAdd);
+    }
+
+    private void addOdd(HashSet<Integer> result, Integer i) {
+        result.add(i);
+        result.add(i*2);
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,18 @@
 package com.epam.mjc.collections.set;
 
 import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        NavigableSet<Integer> squares = new TreeSet<>();
+
+        for(Integer i : sourceList){
+            squares.add(i * i);
+        }
+
+        return squares.subSet(lowerBound, true, upperBound,true );
     }
 }
